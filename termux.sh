a95056c342bbf3cc2a5f144fc607f9e462f2345b
lektop160 <118301747+lektop160@users.noreply.github.com>
2024-07-06 20:25:31 +0600

    Create termux.sh

---
+++ termux.sh
@@ -0,0 +1,11 @@
+# Установка Python и pip
+pkg install python -y
+pkg install python-pip -y
+
+# Установка необходимых Python-библиотек
+pip install requests beautifulsoup4 fake-useragent lxml fuzzywuzzy keyboard
+
+# Установка SQLite
+pkg install sqlite -y
+
+echo "Все необходимые инструменты и библиотеки установлены."
