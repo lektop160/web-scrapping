419c5246ab29ae3df9bc003a2c598e9238d34111
lektop160 <118301747+lektop160@users.noreply.github.com>
2024-07-06 20:56:46 +0600

    Update web-scraping.py

--- web-scraping.py
+++ web-scraping.py
@@ -1,137 +1,99 @@
 import requests
 from bs4 import BeautifulSoup
 import fake_useragent
-from urllib.parse import *
+from urllib.parse import urljoin
 import time
 import sqlite3
 
-# Connect to the SQLite database. If the database does not exist, it will be created.
-conn = sqlite3.connect('kaspi.db')
-cursor = conn.cursor()
+class Scrapper:
+    def __init__(self):
+        self.conn = sqlite3.connect('kaspi.db')
+        self.cursor = self.conn.cursor()
 
-# Create a table named 'scraped_data' if it does not exist.
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS scraped_data (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        url TEXT NOT NULL,
-        title TEXT,
-        date TEXT,
-        content TEXT
-    )
-''')
-conn.commit()
+        # Create a table named 'scraped_data' if it does not exist.
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS scraped_data (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                url TEXT NOT NULL,
+                title TEXT,
+                date TEXT,
+                content TEXT,
+                content_url TEXT NOT NULL
+            )
+        ''')
+        self.conn.commit()
 
-
-def findMainUrl(url, headers):
-    """
-    This function sends a GET request to the specified URL and extracts all 'a href' elements from a specific block.
-    It then processes each extracted URL by calling the 'Scrapping' function.
-
-    Parameters:
-    url (str): The URL to send the GET request to.
-    headers (dict): The headers to include in the GET request.
-
-    Returns:
-    None
-    """
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
-    
-    soup = BeautifulSoup(response.content, 'lxml')
-    block = soup.find('ul', class_="main-left-menu kaspiGuide__product--list")
-    if block:
-        link_elements = block.find_all('a', href=True)
-        print(f"Total 'a href' elements found: {len(link_elements)}")
-        time.sleep(1)
-
-        for link_element in link_elements:
-            href = link_element.get('href')
-            full_url = urljoin(url, href)
-            print(f"Process new URL: {full_url}")
-            Scrapping(full_url, headers)
-    else:
-        print(f"The specified block was not found on the page.      {url}")
-        findMainUrl(url, headers)
-    
-
-def Scrapping(url, headers):
-    """
-    This function sends a GET request to the specified URL and extracts all 'a href' elements from a specific block.
-    It then processes each extracted URL by calling the 'getElements' function.
-
-    Parameters:
-    url (str): The URL to send the GET request to.
-    headers (dict): The headers to include in the GET request.
-
-    Returns:
-    None
-    """
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
-
-    soup = BeautifulSoup(response.content, 'lxml')
-    block = soup.find('ul', id="questionList")
-    if block:
-        link_elements = block.find_all('a', href=True)
-        print(f"Total 'a href' elements found on {url}: {len(link_elements)}")
-
-        for link_element in link_elements:
-            href = link_element.get('href')
-            getElements(href, headers, url)
-    else:
-        print(f"The specified block was not found on the page.      {url}")
-     
+    def find_main_url(self, url, headers):
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
         
-def getElements(url, headers, mainUrl):
-    """
-    This function sends a GET request to the specified URL and extracts specific elements from a specific block.
-    It then inserts the extracted data into the 'scraped_data' table in the SQLite database.
+        soup = BeautifulSoup(response.content, 'lxml')
+        block = soup.find('ul', class_="main-left-menu kaspiGuide__product--list")
+        if block:
+            link_elements = block.find_all('a', href=True)
+            print(f"Total 'a href' elements found: {len(link_elements)}")
+            time.sleep(1)
 
-    Parameters:
-    url (str): The URL to send the GET request to.
-    headers (dict): The headers to include in the GET request.
-    mainUrl (str): The original URL from which the current URL was extracted.
+            for link_element in link_elements:
+                href = link_element.get('href')
+                full_url = urljoin(url, href)
+                print(f"Process new URL: {full_url}")
+                self.scrapping(full_url, headers)
+        else:
+            print(f"The specified block was not found on the page. {url}")
 
-    Returns:
-    None
-    """
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
+    def scrapping(self, url, headers):
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
 
-    soup = BeautifulSoup(response.content, 'lxml')
-    block = soup.find('ul', id="questionList")
-    if block:
-        table = block.find('li', class_='kaspiGuide__question--item expanded_block')
-        h1 = table.find('h1', class_='kaspiGuide__question--title').text
-        date = table.find('span', class_='kaspiGuide__question--description_desktop regular_desc_font').text
-        elements = table.find_all('p')
-        text = ''
-        for element in elements:
-            temp = element.text
-            text += temp + '\n'
-        try:
-            cursor.execute('''
-            INSERT INTO scraped_data (url, title, date, content)
-            VALUES (?, ?, ?, ?)
-            ''', (mainUrl, h1, date, text))
-            conn.commit()
-            print(f"Data inserted successfully for {url}")
-        except sqlite3.Error as e:
-            print(f"Error inserting data: {e}")
+        soup = BeautifulSoup(response.content, 'lxml')
+        block = soup.find('ul', id="questionList")
+        if block:
+            link_elements = block.find_all('a', href=True)
+            print(f"Total 'a href' elements found on {url}: {len(link_elements)}")
 
-    else:
-        print(f"The specified block was not found on the page.      {url}")
-            
+            for link_element in link_elements:
+                href = link_element.get('href')
+                self.get_elements(urljoin(url, href), headers, url)
+        else:
+            print(f"The specified block was not found on the page. {url}")
 
-if __name__ == "__main__":
-    url = "https://guide.kaspi.kz/client/ru"
-    print(f"Process parsing url {url}")
-    
-    user = fake_useragent.UserAgent().random
-    headers = {
-        'User-Agent': user,
-    }
-    
-    findMainUrl(url, headers)  
+    def get_elements(self, url, headers, main_url):
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
 
-conn.close()
+        soup = BeautifulSoup(response.content, 'lxml')
+        block = soup.find('ul', id="questionList")
+        if block:
+            table = block.find('li', class_='kaspiGuide__question--item expanded_block')
+            if table:
+                h1 = table.find('h1', class_='kaspiGuide__question--title').text
+                date = table.find('span', class_='kaspiGuide__question--description_desktop regular_desc_font').text
+                elements = table.find_all('p')
+                text = '\n'.join([element.text for element in elements])
+                
+                try:
+                    self.cursor.execute('''
+                        INSERT INTO scraped_data (url, title, date, content, content_url)
+                        VALUES (?, ?, ?, ?, ?)
+                    ''', (main_url, h1, date, text, url))
+                    self.conn.commit()
+                    print(f"Data inserted successfully for {url}")
+                except sqlite3.Error as e:
+                    print(f"Error inserting data: {e}")
+            else:
+                print(f"No table found in the block. {url}")
+        else:
+            print(f"The specified block was not found on the page. {url}")
+
+    def run(self):
+        url = "https://guide.kaspi.kz/client/ru"
+        print(f"Process parsing url {url}")
+        
+        user = fake_useragent.UserAgent().random
+        headers = {
+            'User-Agent': user,
+        }
+        
+        self.find_main_url(url, headers)
+        self.conn.close()
